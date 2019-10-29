dec086ac75600226926bb6f255b01e1e313a4677
Simon Pratt <simonjamespratt@gmail.com>
2019-10-29 22:50:41 +0000

    initialise

---
+++ include/actlib/helloWorld.hpp
@@ -0,0 +1,19 @@
+//
+//  helloWorld.hpp
+//  AleatoricCompositionTools
+//
+//  Created by Simon Pratt on 29/10/2019.
+//  Copyright © 2019 Simon Pratt. All rights reserved.
+//
+
+#ifndef helloWorld_hpp
+#define helloWorld_hpp
+
+#include <stdio.h>
+#include <string>
+
+namespace actlib::utilities {
+    std::string getHelloWorld();
+}
+
+#endif /* helloWorld_hpp */
---
+++ src/helloWorld.cpp
@@ -0,0 +1,16 @@
+//
+//  helloWorld.cpp
+//  AleatoricCompositionTools
+//
+//  Created by Simon Pratt on 29/10/2019.
+//  Copyright © 2019 Simon Pratt. All rights reserved.
+//
+
+#include "helloWorld.hpp"
+
+namespace actlib::utilities {
+    std::string getHelloWorld()
+    {
+        return "Hello world!";
+    }
+}
