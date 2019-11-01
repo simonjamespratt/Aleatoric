55fb9274b25844365774170d760720a9e7d2a484
Simon Pratt <simonjamespratt@gmail.com>
2019-11-01 20:51:32 +0000

    get classes in there

--- include/actlib/NumberGenerators.hpp
+++ include/actlib/NumberGenerators.hpp
@@ -9,11 +9,81 @@
 #ifndef NumberGenerators_hpp
 #define NumberGenerators_hpp
 
-#include <stdio.h>
 #include <string>
+#include <random>
+
+
 
 namespace actlib::NumberGenerators {
     int addTwoNumbers(int a, int b);
+    
+    struct RandomSelectionRange
+    {
+        int start;
+        int end;
+    };
+    
+    class RandomNumber
+    {
+    public:
+        RandomNumber(int rangeStart, int rangeEnd);
+        ~RandomNumber();
+        
+        int getNumber();
+        
+    private:
+        std::uniform_int_distribution<int> dist;
+    };
+    
+    // Abstract class for derived classes working with ranges, offsets and offering the ability to get a random number
+    class RandomNumberSelector
+    {
+    public:
+        RandomNumberSelector(int rangeStart, int rangeEnd);
+        virtual ~RandomNumberSelector();
+        
+        // NB: removed as pure virtual method because cannot return different types when method has no params
+        // virtual int getNumber() = 0;
+        virtual void reset() = 0;
+        
+    protected:
+        RandomSelectionRange range;
+        int offset;
+        int rangeSize;
+        virtual void initialize() = 0;
+        
+    private:
+    };
+    
+    // Abstract Base class for derived classes using a discrete distribution
+    class DiscreteDistributionRandomNumber : public RandomNumberSelector
+    {
+    public:
+        DiscreteDistributionRandomNumber(int rangeStart, int rangeEnd);
+        virtual ~DiscreteDistributionRandomNumber();
+        
+    protected:
+        std::vector<double> discreteDist;
+        void setUniformDistribution(double value);
+        void setEqualProbability();
+        
+    private:
+    };
+    
+    // Between from RTC
+    class RandomNumberWithinRange : RandomNumberSelector
+    {
+    public:
+        RandomNumberWithinRange(int rangeStart, int rangeEnd);
+        ~RandomNumberWithinRange();
+        int getNumber();
+        void reset() override;
+        
+    protected:
+        void initialize() override;
+        
+    private:
+    };
 }
 
 #endif /* NumberGenerators_hpp */
--- src/NumberGenerators.cpp
+++ src/NumberGenerators.cpp
@@ -13,4 +13,68 @@ namespace actlib::NumberGenerators {
     {
         return a + b;
     }
+    
+    namespace random_number
+    {
+        std::random_device rd;
+        std::mt19937 engine(rd());
+    } // namespace random_number
+    
+    RandomNumber::RandomNumber(int rangeStart, int rangeEnd) : dist(rangeStart, rangeEnd) {}
+    
+    RandomNumber::~RandomNumber() {}
+    
+    int RandomNumber::getNumber()
+    {
+        return dist(random_number::engine);
+    }
+    
+    // RANDOM NUMBER SELECTOR ==============================================================================
+    RandomNumberSelector::RandomNumberSelector(int rangeStart, int rangeEnd)
+    {
+        range.start = rangeStart;
+        range.end = rangeEnd;
+        offset = rangeStart;
+        rangeSize = rangeEnd - rangeStart + 1;
+    }
+    
+    RandomNumberSelector::~RandomNumberSelector() {}
+    
+    // DISCRETE DISTRIBUTION RANDOM NUMBER =================================================================
+    DiscreteDistributionRandomNumber::DiscreteDistributionRandomNumber(int rangeStart, int rangeEnd) : RandomNumberSelector(rangeStart, rangeEnd) {}
+    
+    DiscreteDistributionRandomNumber::~DiscreteDistributionRandomNumber() {}
+    
+    void DiscreteDistributionRandomNumber::setUniformDistribution(double value)
+    {
+        discreteDist.clear();
+        for (int i = 0; i < rangeSize; i++)
+        {
+            discreteDist.push_back(value);
+        }
+    }
+    
+    void DiscreteDistributionRandomNumber::setEqualProbability()
+    {
+        setUniformDistribution(1.0);
+    }
+    
+    // RANDOM WITHIN RANGE =====================================================
+    // range is inclusive, so range is: end - start + 1
+    RandomNumberWithinRange::RandomNumberWithinRange(int rangeStart, int rangeEnd) : RandomNumberSelector(rangeStart, rangeEnd)
+    {
+    }
+    
+    RandomNumberWithinRange::~RandomNumberWithinRange() {}
+    
+    int RandomNumberWithinRange::getNumber()
+    {
+        // select a number in range - uniform dist
+        RandomNumber randomNumber(range.start, range.end);
+        return randomNumber.getNumber();
+    }
+    
+    void RandomNumberWithinRange::reset() {}
+    
+    void RandomNumberWithinRange::initialize() {}
 }
