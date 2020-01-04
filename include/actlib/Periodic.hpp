4f2ce819780db8219d8cca44af6c28c168285167
Simon Pratt <simonjamespratt@gmail.com>
2020-01-04 00:22:15 +0000

    Periodic protocol implementation

---
+++ include/actlib/Periodic.hpp
@@ -0,0 +1,37 @@
+#ifndef Periodic_hpp
+#define Periodic_hpp
+
+#include "IDiscreteGenerator.hpp"
+#include "ProtocolInterface.hpp"
+#include "Range.hpp"
+
+namespace actlib { namespace Numbers {
+class Periodic : public Protocol {
+  public:
+    Periodic(IDiscreteGenerator &generator,
+             Range &range,
+             double chanceOfRepetition);
+
+    Periodic(IDiscreteGenerator &generator,
+             Range &range,
+             double chanceOfRepetition,
+             int initialSelection);
+
+    ~Periodic();
+
+    int getNumber() override;
+    void reset() override;
+
+  private:
+    Range &m_range;
+    IDiscreteGenerator &m_generator;
+    double m_periodicity;
+    double m_remainderAllocation;
+    void setPeriodicDistribution(int selectedIndex);
+    int m_initialSelection;
+    bool m_haveInitialSelection;
+    bool m_haveRequestedFirstNumber;
+};
+}} // namespace actlib::Numbers
+
+#endif /* Periodic_hpp */
